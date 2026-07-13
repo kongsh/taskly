cdd9aad2c96c866753649bbaa6bae663f64c4bc3
kongsh <ksh080838@gmail.com>
2026-07-13 19:11:12 +0900

    feat: add new task dialog UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,19 @@
 
 import { TaskList } from "@/components/task/TaskList";
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Field, FieldGroup } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import {
   Select,
   SelectContent,
@@ -10,6 +22,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
 import { tasks } from "@/data/tasks";
 import { StatusFilter, TaskSortOrder } from "@/types/task";
 import { CirclePlus } from "lucide-react";
@@ -126,13 +139,68 @@ export default function Home() {
           </Select>
         </div>
 
-        <Button
-          variant="outline"
-          className="flex items-center justify-center gap-2"
-        >
-          <CirclePlus className="size-4" />
-          New Task
-        </Button>
+        <Dialog>
+          <DialogTrigger
+            type="button"
+            className="flex items-center gap-2 border p-1.5 rounded-lg h-8 text-base hover:bg-muted"
+          >
+            <CirclePlus className="size-4" />
+            New Task
+          </DialogTrigger>
+          <DialogContent>
+            <form>
+              <DialogHeader>
+                <DialogTitle>Create New Task</DialogTitle>
+                <DialogDescription>새 Task를 작성해보세요.</DialogDescription>
+              </DialogHeader>
+              <FieldGroup>
+                <Field>
+                  <Label htmlFor="title">Task 제목</Label>
+                  <Input id="title" name="title" placeholder="Task 제목" />
+                </Field>
+                <Field>
+                  <Label htmlFor="description">설명</Label>
+                  <Textarea
+                    id="description"
+                    name="description"
+                    placeholder="Task 설명란"
+                  />
+                </Field>
+                <Field>
+                  <Label htmlFor="status">현재 상태</Label>
+                  <Select id="status" name="status">
+                    <SelectTrigger>
+                      <SelectValue placeholder="준비 중" />
+                    </SelectTrigger>
+                    <SelectContent alignItemWithTrigger={false}>
+                      {selectStatusItems
+                        .filter((item) => item.value !== "all")
+                        .map((item) => (
+                          <SelectItem key={item.value} value={item.value}>
+                            {item.label}
+                          </SelectItem>
+                        ))}
+                    </SelectContent>
+                  </Select>
+                </Field>
+                <Field>
+                  <Label htmlFor="dueDate">마감 기한(D-day)</Label>
+                  <Input type="number" id="dueDate" name="dueDate" />
+                </Field>
+              </FieldGroup>
+              <DialogFooter>
+                <Button type="submit" variant="outline">
+                  Add Task
+                </Button>
+                <DialogClose>
+                  <Button type="button" variant="destructive">
+                    Cancel
+                  </Button>
+                </DialogClose>
+              </DialogFooter>
+            </form>
+          </DialogContent>
+        </Dialog>
       </div>
       <TaskList tasks={sortedTasks} />
     </div>
